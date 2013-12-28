5dd591b647b2eb4dbe1f51d6048e69b769346471
Robert Zenz <bobby@bonsaimind.org>
2013-12-28 15:58:42 +0100

    Upped Clojure version to 1.5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ClosedDoor "1.0.0"
   :description "A simple PHP like text templating system."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :main closeddoor.core)
